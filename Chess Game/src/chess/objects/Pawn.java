29cc611bf46c456d3c23c55ac49a2fd3c4145fb4
lordstevex <lordstevex@gmail.com>
2015-09-29 12:01:14 -0400

    Removed constructors for the Pawn class, we probably don't need

--- Chess Game/src/chess/objects/Pawn.java
+++ Chess Game/src/chess/objects/Pawn.java
@@ -1,56 +1,19 @@
 package chess.objects;
 
 public class Pawn extends Piece {
-	
-	/**
-	 * If the Pawn has moved it can only go one spot, otherwise it can go 2
-	 * (implemented as protected variable in Piece
-	 */
-//	private boolean hasMoved;
-	
-	
-	/*Local instance variables are not needed - use protected
-	 * instances from parent class
-	 */
-	//private final static String NAME = "P";
-	
-	/**
-	 * Constructor for Pawn that initializes hasMoved to false.
-	 * 
-	 * @param color is the color of the Pawn
-	 * @param isAlive is whether the pawn is alive
-	 */
-	public Pawn(PColor color, boolean isAlive) {
-		super(color, isAlive, "Pawn");
-		hasMoved = false;
-		if(color.equals(PColor.White))
-			this.icon = "\u2659";
-		else
-			this.icon = "\u265f";
-	}
-	
-	/**
-	 * Secondary constructor that sets color, isAlive, and hasMoved
-	 * 
-	 * @param color is the color of the Pawn
-	 * @param isAlive is whether the pawn is alive
-	 * @param hasMoved is whether the pawn has moved
-	 */
-	public Pawn(PColor color, boolean isAlive, boolean hasMoved) {
-		super(color, isAlive);
-		this.hasMoved = hasMoved;
-		if(color.equals(PColor.White))
-			this.icon = "\u2659";
-		else
-			this.icon = "\u265f";
-	}
-	
-//	public boolean isHasMoved(){
-//		return hasMoved;
-//	}
-//	
-//	public void setHasMoved(boolean moved) {
-//		this.hasMoved = moved;
-//	}
 
+	/*******************************************************************
+	 * Constructor for the Pawn that takes it's PColor
+	 * 
+	 * @param color
+	 *            is the PColor of the Pawn to make
+	 ******************************************************************/
+	public Pawn(PColor color) {
+		super(color, true, "Pawn");
+		hasMoved = false;
+		if (color.equals(PColor.White))
+			this.icon = "\u2659";
+		else
+			this.icon = "\u265f";
+	}
 }
