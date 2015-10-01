a740a845658162bb6a4dafc1b3e48722c3ffac1a
NexusConvoy <andvuo@gmail.com>
2015-10-01 14:42:08 -0400

    Added Turn feature and an untested CheckWin Method

--- Chess Game/src/chess/controller/ChessController.java
+++ Chess Game/src/chess/controller/ChessController.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 
 import chess.gui.ChessGUI;
 import chess.main.Chess;
+import chess.objects.King;
+import chess.objects.PColor;
 
 public class ChessController {
 
@@ -19,6 +21,9 @@ public class ChessController {
 
 	/** Track whether it is the first click for that player's turn */
 	private boolean firstClick;
+	
+
+	private boolean WhiteTurn;
 
 	/*******************************************************************
 	 * Constructor for the Controller part of the MVC Takes in both the
@@ -40,6 +45,7 @@ public class ChessController {
 		this.gui = gui;
 		this.game = game;
 		this.firstClick = true;
+		WhiteTurn = true;
 
 		this.gui.addChessListener(new ChessListener());
 	}
@@ -57,17 +63,48 @@ public class ChessController {
 						c2 = col;
 						firstClick = true;
 					}
-					System.out.println("The Row is: " + (row + 1)
-							+ " and the Column is: " + (col + 1));
+					//System.out.println("The Row is: " + (row + 1)
+							//+ " and the Column is: " + (col + 1));
 				}
 			}
 		}
 	}
 
+	private int CheckWin(){
+		boolean WhiteKingAlive = false;
+		boolean BlackKingAlive = false;
+		
+		int Winner = -1;
+		for (int x = 0; x<8; x++){
+			for (int y = 0; y<8; y++){
+				if (game.getPieceAt(x, y) != null){
+					if (game.getPieceAt(x, y) instanceof King){
+						if ((game.getPieceAt(x, y).getColor().equals(PColor.White))){
+							WhiteKingAlive = true;
+						}
+						else
+							BlackKingAlive = true;
+					}
+				}
+			}
+				
+		}
+		
+		if (!BlackKingAlive){
+			Winner = 0;
+		}
+		else if (!WhiteKingAlive){
+			Winner = 1;
+		}
+		
+		return Winner;
+	}
+	
 	class ChessListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			int Winner = -1;
 			if(e.getSource() == gui.getNewItem()) {
 				game.reset();
 				gui.resetBoard();
@@ -77,23 +114,50 @@ public class ChessController {
 			} else if(e.getSource() == gui.getExitItem()) {
 				System.exit(0);
 			}
-			// Very basic method to find the button and piece and checks
-			// if it can move there
-			if (firstClick) {
-				findCell(e);
-			} else {
-				findCell(e);
-				System.out.println(r2 + 1 + " " + (c2 + 1));
-				if (game.checkMove(r1, c1, r2, c2,
-						game.getPieceAt(r1, c1))) {
-							gui.getButtonAt(r1, c1).setText("");
-								if (game.getPieceAt(r2, c2) != null) {
-									gui.getButtonAt(r2, c2).setText(
-											game.getPieceAt(r2, c2).getIcon());
-									gui.revalidate();
+			
+			
+				// Very basic method to find the button and piece and checks
+				// if it can move there
+				if (firstClick) {
+					findCell(e);
+					if(game.getPieceAt(r1, c1) != null){
+						if (WhiteTurn){
+							if (!game.getPieceAt(r1, c1).getColor().equals(PColor.White)){
+								System.out.println("BLACK PIECE WAS CLICKED WHEN IT WAS SUPPOSED TO BE WHITE");
+								firstClick = true;
+						}
+					}
+						else if (!WhiteTurn){
+							if (game.getPieceAt(r1, c1).getColor().equals(PColor.White)){
+								System.out.println("WHITE PIECE WAS CLICKED WHEN IT WAS SUPPOSED TO BE BLACK");
+								firstClick = true;
+						}
 					}
 				}
+					else
+						firstClick = true;
+			} else {
+					System.out.println("Second Click and WhiteTurn is " + WhiteTurn);
+					findCell(e);
+					//System.out.println(r2 + 1 + " " + (c2 + 1));
+					if (game.checkMove(r1, c1, r2, c2,
+							game.getPieceAt(r1, c1))) {
+						WhiteTurn = !WhiteTurn;
+								gui.getButtonAt(r1, c1).setText("");
+									if (game.getPieceAt(r2, c2) != null) {
+										gui.getButtonAt(r2, c2).setText(
+											game.getPieceAt(r2, c2).getIcon());
+										gui.revalidate();
+									}
+				}
 			}
+			Winner = CheckWin();
+			
 		}
+		//Insert Check King Here
+		
+		
 	}
+	
+	
 }
