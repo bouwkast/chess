9a4ccd1b07f85468184be0e8104352d50771c1a1
NexusConvoy <andvuo@gmail.com>
2015-10-07 10:30:02 -0400

    Added dialog boxes when a user wins and when the wrong colored piece is selected

--- Chess Game/src/chess/controller/ChessController.java
+++ Chess Game/src/chess/controller/ChessController.java
@@ -3,6 +3,8 @@ package chess.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
 import chess.gui.ChessGUI;
 import chess.main.Chess;
 import chess.objects.King;
@@ -25,6 +27,9 @@ public class ChessController {
 
 	private boolean WhiteTurn;
 
+	private boolean WhiteKingAlive = false;
+	
+	private boolean BlackKingAlive = false;
 	/*******************************************************************
 	 * Constructor for the Controller part of the MVC Takes in both the
 	 * GUI and the game as parameters and adds the ChessListener onto
@@ -73,7 +78,11 @@ public class ChessController {
 	private int CheckWin(){
 		boolean WhiteKingAlive = false;
 		boolean BlackKingAlive = false;
-		
+		String Options[] = new String[3];
+		Options[0] = "New Game";
+		Options[1] = "Quit";
+		Options[2] = "Cancel";
+		int choice = -1;
 		int Winner = -1;
 		for (int x = 0; x<8; x++){
 			for (int y = 0; y<8; y++){
@@ -92,9 +101,71 @@ public class ChessController {
 		
 		if (!BlackKingAlive){
 			Winner = 0;
+			choice = JOptionPane.showOptionDialog(gui, "Game Over! "
+					+ "Player 1 Wins!", "Player 1 Wins"
+					,0,JOptionPane.INFORMATION_MESSAGE,null,Options,null);			
+			if (choice ==  0){
+				game.reset();
+				gui.resetBoard();
+				gui.revalidate();
+				gui.repaint();
+				firstClick = true;
+				WhiteTurn = true;
+				WhiteKingAlive = true;
+				BlackKingAlive = true;
+
+				for (int x = 0; x<8; x++){
+					for (int y = 0; y<8; y++){
+						gui.getButtonAt(x, y).setEnabled(true);
+					}
+				}
+				
+				
+			}
+			else if (choice == 1){
+				gui.dispose();
+			}
+			else if (choice == 2){
+				for (int x = 0; x<8; x++){
+					for (int y = 0; y<8; y++){
+						gui.getButtonAt(x, y).setEnabled(false);
+					}
+				}
+			}
+		
+			
 		}
 		else if (!WhiteKingAlive){
 			Winner = 1;
+			choice = JOptionPane.showOptionDialog(gui, "Game Over! Player 2 Wins!", "Player 2 Wins"
+					,0,JOptionPane.INFORMATION_MESSAGE,null,Options,null);
+			if (choice ==  0){
+				game.reset();
+				gui.resetBoard();
+				gui.revalidate();
+				gui.repaint();
+				firstClick = true;
+				WhiteTurn = true;
+				WhiteKingAlive = true;
+				BlackKingAlive = true;
+				
+				for (int x = 0; x<8; x++){
+					for (int y = 0; y<8; y++){
+						gui.getButtonAt(x, y).setEnabled(true);
+					}
+				}
+			}
+			else if (choice == 1){
+				gui.dispose();
+			}
+			else if (choice == 2){
+				for (int x = 0; x<8; x++){
+					for (int y = 0; y<8; y++){
+						gui.getButtonAt(x, y).setEnabled(false);
+					}
+				}
+			}
+			
 		}
 		
 		return Winner;
@@ -111,6 +182,18 @@ public class ChessController {
 				gui.revalidate();
 				gui.repaint();
 				
+				firstClick = true;
+				WhiteTurn = true;
+				WhiteKingAlive = true;
+				BlackKingAlive = true;
+				
+				for (int x = 0; x<8; x++){
+					for (int y = 0; y<8; y++){
+						gui.getButtonAt(x, y).setEnabled(true);
+					}
+				}
+				
+				
 			} else if(e.getSource() == gui.getExitItem()) {
 				System.exit(0);
 			}
@@ -123,13 +206,15 @@ public class ChessController {
 					if(game.getPieceAt(r1, c1) != null){
 						if (WhiteTurn){
 							if (!game.getPieceAt(r1, c1).getColor().equals(PColor.White)){
-								System.out.println("BLACK PIECE WAS CLICKED WHEN IT WAS SUPPOSED TO BE WHITE");
+								JOptionPane.showMessageDialog(gui, "Invalid selection. It is player"
+										+ "1's turn.");
 								firstClick = true;
 						}
 					}
 						else if (!WhiteTurn){
 							if (game.getPieceAt(r1, c1).getColor().equals(PColor.White)){
-								System.out.println("WHITE PIECE WAS CLICKED WHEN IT WAS SUPPOSED TO BE BLACK");
+								JOptionPane.showMessageDialog(gui, "Invalid selection. It is player"
+										+ " 2's turn.");
 								firstClick = true;
 						}
 					}
